da4ad54f989eb2e0d995908b9f8891ed16bb123b
robertsavian <robertsavian@gmail.com>
2020-12-03 19:15:01 -0600

    Controlled & Uncontrolled Component

--- src/components/pages/about-page/AboutPage.tsx
+++ src/components/pages/about-page/AboutPage.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { Routes } from '../../../constants/Routes';
+import { UiSelect } from '../../ui/ui-select/UiSelect';
 
 interface IProps {}
 
 export const AboutPage: React.FC<IProps> = (props) => {
+  const [controlledValue, setControlledValue] = useState<string>('3');
+
   return (
     <div>
       <h1>About</h1>
       <p>This is the about page</p>
+      <div>
+        <UiSelect
+          id="uncontrolled"
+          onChange={(value) => console.log(value, typeof value)}
+          initialValue="2"
+          options={[
+            { label: 'One', value: '1' },
+            { label: 'Two', value: '2' },
+            { label: 'Three', value: '3' },
+            { label: 'Four', value: '4' },
+          ]}
+        />
+        <UiSelect
+          id="controlled"
+          onChange={(value) => setControlledValue(value)}
+          value={controlledValue}
+          options={[
+            { label: 'One', value: '1' },
+            { label: 'Two', value: '2' },
+            { label: 'Three', value: '3' },
+            { label: 'Four', value: '4' },
+          ]}
+        />
+      </div>
       <p>
         <Link href={Routes.Index}>
           <a>Go home</a>
--- src/components/pages/index-page/IndexPage.tsx
+++ src/components/pages/index-page/IndexPage.tsx
@@ -1,13 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { Routes } from '../../../constants/Routes';
-import { UiSelect } from '../../ui/ui-select/UiSelect';
 
 interface IProps {}
 
 export const IndexPage: React.FC<IProps> = (props) => {
-  const [controlledValue, setControlledValue] = useState<string>('3');
-
   return (
     <div>
       <h1>
@@ -16,30 +13,6 @@ export const IndexPage: React.FC<IProps> = (props) => {
           👋
         </span>
       </h1>
-      <div>
-        <UiSelect
-          id="uncontrolled"
-          onChange={(value) => console.log(value, typeof value)}
-          initialValue="2"
-          options={[
-            { label: 'One', value: '1' },
-            { label: 'Two', value: '2' },
-            { label: 'Three', value: '3' },
-            { label: 'Four', value: '4' },
-          ]}
-        />
-        <UiSelect
-          id="controlled"
-          onChange={(value) => setControlledValue(value)}
-          value={controlledValue}
-          options={[
-            { label: 'One', value: '1' },
-            { label: 'Two', value: '2' },
-            { label: 'Three', value: '3' },
-            { label: 'Four', value: '4' },
-          ]}
-        />
-      </div>
       <p>
         <Link href={Routes.About}>
           <a>About</a>
